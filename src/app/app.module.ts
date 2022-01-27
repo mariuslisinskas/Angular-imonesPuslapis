269d8309cb4f3d94301d501a1ac0af8aa0663c5d
superMario737 <82049062+mariuslisinskas@users.noreply.github.com>
2022-01-27 16:54:06 +0200

    First Upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ApieComponent } from './apie/apie.component';
+import { PaslaugosComponent } from './paslaugos/paslaugos.component';
+import { DarbaiComponent } from './darbai/darbai.component';
+import { KontaktaiComponent } from './kontaktai/kontaktai.component';
+import { RouterModule, Routes } from '@angular/router';
+import { SwiperModule } from 'swiper/angular';
+
+const appRoutes:Routes=[
+    {path:"", component:ApieComponent},
+    {path:"paslaugos", component:PaslaugosComponent},
+    {path:"darbai", component:DarbaiComponent},
+    {path:"kontaktai", component:KontaktaiComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApieComponent,
+    PaslaugosComponent,
+    DarbaiComponent,
+    KontaktaiComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    SwiperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
